f73cfb3abfff0a3dcaabf1d981d6fee7b3db112a
RAPHAEL <80759812+C4PISTRANO@users.noreply.github.com>
2023-10-26 11:32:59 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,31 +1,8 @@
-# Portfolio Raphael Capistrano
+# Portfolio 2.0 Raphael Capistrano
 <a href="https://raphaelcapistrano.dev.br/">
 <img src="https://github.com/C4PISTRANO/Portfolio/blob/main/src/img/BannerRaphaelCapistrano.png">
 </a>
 
-## :memo: Sobre 
-Portfolio pessoal, onde falo um pouco sobre mim, apresento meus serviços, projetos e contatos.
-
-Nesse projeto além da codificação da página, aprendi sobre criação e registro de domínio, hospedagem, 
-criação e configuração de e-mail personalizado, utilizei ferramentas para testes e fiz a otimização de desempenho, 
-fiz a otimização para os mecanismos de busca - SEO e aprendi a fazer a indexação do meu site para a pesquisa no Google.
-
-Foram utilizadas funções para o menu de navegação e ativar/desativar conteúdo, layout
-responsivo, estilização com glassmorfismo, menu de navegação, transição suave entre
-as seções, consumo de API do WhatsApp.
-
 ## :earth_americas: Publicação
 
-Você pode acessar o Portfolio clicando em [https://raphael.capistrano.dev.br](https://raphaelcapistrano.dev.br/)
-
-## :stopwatch: Desempenho
-
-Teste de desempenho do meu portfolio, utilizando a ferramenta PageSpeed.
-Feito otimização do código-fonte, tipos de imagens, meta tags, acessibilidade, responsividade e melhores práticas de desenvolvimento.
-
-O site está como o melhor desempenho possível.
-![PreviewDesempenho1](https://github.com/C4PISTRANO/Portfolio/assets/80759812/4960bd65-d96d-45f6-9189-71474e777d2f)
-
-Acesse em: https://pagespeed.web.dev/analysis/https-raphaelcapistrano-dev-br/ag5t7y8lga?form_factor=desktop 
-
-🇨 🇦 🇵 🇮 🇸 🇹 🇷 🇦 🇳 🇴
+Você pode acessar o Portfolio clicando em [Raphael Capistrano](https://raphaelcapistrano.dev.br/).
--- src/js/script.js
+++ src/js/script.js
@@ -1,25 +1,65 @@
 let list = document.querySelectorAll('.nav li');
-function active(){
-    list.forEach((i) => i.classList.remove('active'));
-    this.classList.add('active');
-}
-list.forEach((i) => i.addEventListener('click',active));
-
-let menuToggle = document.querySelector('.menuToggle');
 let header = document.querySelector('header');
-menuToggle.onclick = function(){
+
+/*mark menu item corresponding to visible section*/
+function highlightMenu() {
+    list.forEach((item) => {
+        const sectionId = item.querySelector('a').getAttribute('href').substring(1);
+        const section = document.getElementById(sectionId);
+        const rect = section.getBoundingClientRect();
+
+        if (rect.top <= 80 && rect.bottom >= 0) {
+            list.forEach((i) => i.classList.remove('active'));
+            item.classList.add('active');
+        }
+    });
+}
+
+/*highlight the menu while the user scrolls*/
+window.addEventListener('scroll', highlightMenu);
+
+/*open hamburger menu when click on hamburger icon*/
+function toggleHeader() {
     header.classList.toggle('active');
 }
 
+let menuToggle = document.querySelector('.menuToggle');
+menuToggle.addEventListener('click', toggleHeader);
+
+/*close hamburger menu when select menu item*/
+function closeHeader() {
+    header.classList.remove('active');
+}
+
+let menuClose = document.querySelector('.nav')
+menuClose.addEventListener('click', closeHeader);
+
+/*button show-or-hide in projects section*/
 var btn = document.querySelector('#show-or-hide');
 var content2 = document.querySelector('.content2');
 
-btn.addEventListener('click', function() {
-    if(content2.style.display === 'block' || content2.style.display === 'grid') {
+btn.addEventListener('click', function () {
+    if (content2.style.display === 'block' || content2.style.display === 'grid') {
         content2.style.display = 'none';
-        
+        this.textContent = 'Mais Projetos';
     }
     else {
         content2.style.display = 'grid';
+        this.textContent = 'Menos Projetos';
+        this.attr = 'Clique para ver menos projetos.';
     }
 })
+
+/*save choice of cookies msg*/
+var msgCookies = document.getElementById('cookies-msg');
+
+function accept() {
+    localStorage.lgpdMsg = "sim"
+    msgCookies.classList.remove('show-msg')
+}
+if (localStorage.lgpdMsg == 'sim') {
+    msgCookies.classList.remove('show-msg')
+}
+else {
+    msgCookies.classList.add('show-msg')
+}
